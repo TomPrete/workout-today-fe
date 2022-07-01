c3843cf4485462c4a2b29c382de76fec85ca2fc6
tomprete <prete316@gmail.com>
2022-07-01 10:55:07 -0500

    added the ability to pause

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,15 +3,22 @@ let totalSeconds = 2700;
 
 const Timer = () => {
   const [seconds, setSeconds] = useState(2700);
+  const [isPaused, setIsPaused] = useState(false);
 
 
   useEffect(() => {
-    if (seconds > 0) {
-      setTimeout(() => setSeconds(seconds - 1), 1000)
-    } else {
-      setSeconds("Next Exercise")
+    if (!isPaused) {
+      if (seconds > 0) {
+        setTimeout(() => setSeconds(seconds - 1), 1000)
+      } else {
+        setSeconds("Next Exercise")
+      }
     }
   });
+
+  const pauseTimer = () => {
+    setIsPaused(!isPaused)
+  }
 
   const calculateTime = (seconds) => {
     let remainingMinutes = Math.floor(seconds / 60)
@@ -26,6 +33,7 @@ const Timer = () => {
     <div>
       {/*<p>{`${minutes}:${seconds % 60}`}</p>*/}
       <p>{ `${calculateTime(seconds)[0]}:${calculateTime(seconds)[1]}` }</p>
+      <button onClick={pauseTimer}>{isPaused ? "Resume" : "Pause"}</button>
     </div>
   );
 };
